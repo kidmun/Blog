ba8b4d04644d333fd74550a321b099e8cfeb0c59
kidmun <kidusguade41@gmail.com>
2024-08-15 23:12:53 +0300

    add signup and login

--- cmd/main.go
+++ cmd/main.go
@@ -3,18 +3,19 @@ package main
 import (
 	"Blog/api/route"
 	"Blog/bootstrap"
+	
 	"time"
 
 	"github.com/gin-gonic/gin"
 )
-
 func main() {	
+
 	app := bootstrap.App()
 	env := app.Env
 	db := app.Mongo.Database(env.DBName)
 	defer app.CloseDBConnection()
 	timeout := time.Duration(env.ContextTimeout) * time.Second
 	gin := gin.Default()
-	route.Setup(timeout, db, gin)
+	route.Setup(env, timeout, db, gin)
 	gin.Run(env.ServerAddress)
 }
